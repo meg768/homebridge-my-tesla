6a7529c0008a60a8c55ee0fc8ed83cdf729c1be5
meg768 <meg768@users.noreply.github.com>
2019-10-04 17:38:09 +0200

    -

--- src/defrost-service.js
+++ src/defrost-service.js
@@ -8,45 +8,44 @@ module.exports = class extends Service.Switch {
     constructor(tesla, name) {
         super(name, "defrost");
 
-        this.enabled = false;
-        this.tesla = tesla;
-        this.log = tesla.log;
-        
+        var defrostActive = false;
+        var log = tesla.log;
+
         this.getCharacteristic(Characteristic.On).on('get', (callback) => {
-            callback(null, this.enabled);    
+            callback(null, defrostActive);    
         });
     
         this.getCharacteristic(Characteristic.On).on('set', (value, callback) => {
-            callback(null, this.state = value);    
+            callback(null, defrostActive = value);    
         });
 
         var loop = () => {
             var vin = tesla.config.vin;
 
-            if (!this.enabled) {
-                this.log(`Defrost not active...`);
+            if (!defrostActive) {
+                log(`Defrost not active...`);
                 return setTimeout(loop.bind(this), 60000);
             }
 
-            this.log(`Checking temperature for vehicle ${vin}...`);
+            log(`Checking temperature for vehicle ${vin}...`);
 
             tesla.api.wakeUp(vin).then(() => {
                 return tesla.api.getVehicleData(vin);         
             })
             .then((response) => {
                 if (response && response.climate_state && response.climate_state.inside_temp < 4) {
-                    this.log('Starting air conditioner.');
+                    log('Starting air conditioner.');
                     return Promise.resolve();
                     return tesla.api.autoConditioningStart(vin);
                 }
                 else {
-                    this.log('Stopping air conditioner.');
+                    log('Stopping air conditioner.');
                     return Promise.resolve();
                     return tesla.api.autoConditioningStop(vin);    
                 }
             })
             .catch((error) => {
-                this.log(error);
+                log(error);
             })
             .then(() => {
                 setTimeout(loop.bind(this), 60000);
