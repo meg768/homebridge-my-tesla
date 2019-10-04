47b410c17208dc249425c8cfc0db72b618b47c5e
meg768 <meg768@users.noreply.github.com>
2019-10-04 20:15:43 +0200

    -

--- src/temperature-service.js
+++ src/temperature-service.js
@@ -7,13 +7,21 @@ module.exports = class extends Service.TemperatureSensor {
     constructor(tesla, name) {
         super(name, "inner-temperature");
 
+        var getInsideTemperature = (response) => {
+            if (response && response.climate_state && response.climate_state.inside_temp)
+                return response.climate_state.inside_temp;
+                
+            return 20;
+        };
+
+        this.on('update', (response) => {                
+            this.getCharacteristic(Characteristic.On).updateValue(getInsideTemperature(response));
+        });
+
         this.getCharacteristic(Characteristic.CurrentTemperature).on('get', (callback) => {
 
             tesla.refresh((response) => {
-                if (response && response.climate_state && response.climate_state.inside_temp != undefined)
-                    callback(null, response.climate_state.inside_temp);
-                else
-                    callback(null);
+                callback(null, getInsideTemperature(response));
             });
 
         });
