4dad802fb1a12129168011863bc8c0fd173e6453
meg768 <meg768@users.noreply.github.com>
2019-11-01 23:02:57 +0100

    -

--- src/platform.js
+++ src/platform.js
@@ -36,8 +36,6 @@ module.exports = class Platform {
 
     constructor(log, config, homebridge) {
 
-        log(config);
-
         this.config = config;
         this.log = log;
         this.homebridge = homebridge;
@@ -66,35 +64,7 @@ module.exports = class Platform {
 
     }
 
-    pushover(payload) {
-
-        var Pushover = require('pushover-notifications');
-    	var user     = process.env.PUSHOVER_USER;
-    	var token    = process.env.PUSHOVER_TOKEN;
-
-    	if (user && token) {
-			try {
-                payload = Object.assign({priority:0}, payload);
-
-                if (payload.message && payload.message.length > 0) {
-    				var push = new Pushover({user:user, token:token});
-
-    				push.send(payload, function(error, result) {
-    					if (error) {
-    						this.log(error.stack);
-    					}
-    				});
-
-                }
-			}
-			catch(error) {
-				this.log('Failed to send Pushover notification.', error.message);
-    		};
-
-    	}
-
-    };
-
+    
     addAccessory(accessory) {
         console.log('Adding to accessory arrayt');
         this.accessoryArray.push(accessory);
@@ -104,10 +74,6 @@ module.exports = class Platform {
     accessories() {
         console.log('::::::::::::::::::::: accessories called', this.accessoryArray.length);
         return this.accessoryArray;
-    }
-
-    generateUUID(id) {
-        return this.homebridge.hap.uuid.generate(id.toString());
     }
 
 
