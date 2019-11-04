21c8d8e98dbdf8d1e843a36f8ee877a4a4d4fbd4
meg768 <meg768@users.noreply.github.com>
2019-11-04 20:36:45 +0100

    -

--- src/vehicle.js
+++ src/vehicle.js
@@ -28,19 +28,19 @@ module.exports = class Tesla extends Events  {
         var TemperatureAccessory = require('./accessories/temperature.js');
         var DefrostAccessory = require('./accessories/defrost.js');
 
-        if (this.config.locks)
+        if (this.config.locks && this.config.locks.enabled)
             this.addAccessory(new DoorLockAccessory({vehicle:this, config:this.config.locks}));
 
-        if (this.config.charging)
+        if (this.config.charging && this.config.charging.enabled)
             this.addAccessory(new ChargingAccessory({vehicle:this, config:this.config.charging}));
 
-        if (this.config.hvac)
+        if (this.config.hvac && this.config.hvac.enabled)
             this.addAccessory(new AirConditioningAccessory({vehicle:this, config:this.config.hvac}));
 
-        if (this.config.temperature)
+        if (this.config.temperature && this.config.temperature.enabled)
             this.addAccessory(new TemperatureAccessory({vehicle:this, config:this.config.temperature}));
 
-        if (this.config.defrost)
+        if (this.config.defrost && this.config.defrost.enabled)
             this.addAccessory(new DefrostAccessory({vehicle:this, config:this.config.defrost}));
 
         var configLoginOptions = {username:config.username, password:config.password, clientID:config.clientID, clientSecret:config.clientSecret};
