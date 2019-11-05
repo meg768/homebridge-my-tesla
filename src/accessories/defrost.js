ff77e62ce1a5bd1ee8f903e9a38240fd2f1bfe91
meg768 <meg768@users.noreply.github.com>
2019-11-05 20:42:08 +0100

    -

--- src/accessories/defrost.js
+++ src/accessories/defrost.js
@@ -12,7 +12,7 @@ module.exports = class extends Accessory {
         super(options);
 
         var defaultConfig = {
-            temperatureRange: [5, 10],
+            temperatureRange: [5, 101,
             temperatureCheckFrequency: 15,
             minBatteryLevel: 60,
         };
