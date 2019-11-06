a0499d7d1eca1f642485fecaf2a32dc9aaf26083
meg768 <meg768@users.noreply.github.com>
2019-11-06 18:52:47 +0100

    -

--- src/tesla-api.js
+++ src/tesla-api.js
@@ -227,6 +227,7 @@ module.exports = class API {
 
         // Check if called with in reasonable time
         if (this.lastResponse && isDate(this.lastResponse.timestamp) && (now.valueOf() - this.lastResponse.timestamp.valueOf() < wakeupInterval)) {
+            this.debug('Returning cached result from wakeUp().');
             return Promise.resolve(this.lastResponse.data);
         }
 
