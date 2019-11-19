4a0cbde5bbaac08e711b94afcd0201ea19976df5
meg768 <meg768@users.noreply.github.com>
2019-11-19 18:51:15 +0100

    -

--- src/accessories/ping.js
+++ src/accessories/ping.js
@@ -4,6 +4,149 @@ var Characteristic  = require('../homebridge.js').Characteristic;
 var Timer = require('yow/timer');
 var Accessory = require('../accessory.js');
 
+class Switch {
+
+    constructor(name, uuid) {
+        this.service = new Service.Switch(name, uuid);
+        this.value = undefined;
+
+        this.service.getCharacteristic(Characteristic.On).on('get', (callback) => {
+            callback(null, this.getValue());
+        });
+    
+        this.service.getCharacteristic(Characteristic.On).on('set', (value, callback) => {
+            this.setValue(value).then(() => {
+                callback(null, this.getValue());
+            })
+            .catch((error) => {
+                this.log(error);
+                callback(null);
+            })
+        });
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    updateValue() {
+        this.service.getCharacteristic(Characteristic.On).updateValue(this.value);
+        return Promise.resolve();
+    }
+
+    setValue(value) {
+        value = value ? true : false;
+
+        return new Promise((resolve, reject) => {
+
+            Promise.resolve().then(() => {
+                if (this.getValue() === value) {
+                }
+                else {
+                    this.debug(`Setting switch "${this.name}" state to "${value}".`);
+                    this.value = value;
+                    this.emit('valueChanged', this.getValue());
+                }
+                return Promise.resolve();
+            })
+            .then(() => {
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    
+        })
+    }
+
+    
+}
+
+
+
+
+
+
+
+module.exports = class extends Accessory {
+
+    constructor(options) {
+
+        var defaultConfig = {
+            name: 'Ping',
+            requiredBatteryLevel : 40,
+            timerInterval : 5,
+            enabled: true
+        };
+
+        var {config, ...options} = options;
+        super({...options, config:{...defaultConfig, ...config}});
+
+        this.requiredBatteryLevel   = config.requiredBatteryLevel;
+        this.timer                  = new Timer();
+        this.timerInterval          = this.config.timerInterval * 60000;
+
+        this.switch = new Switch(this.name);
+
+        this.addService(this.switch.service);
+
+        this.switch.on('valueChanged', (value) => {
+            if (value)
+                this.ping();
+        });
+
+        this.vehicle.on('vehicleData', (vehicleData) => {
+
+            Promise.resolve().then(() => {
+                if (vehicleData.getBatteryLevel() < this.requiredBatteryLevel) {
+                    this.log(`Battery level too low for ping to be enabled. Turning off.`);
+                    return this.switch.setValue(false);
+                }
+                else   
+                    return Promise.resolve();
+    
+            })
+            .then(() => {
+                return this.switch.updateValue();
+            })
+            .catch((error) => {
+                this.log(error);
+            });
+        });
+
+        // Listen to responses from Tesla API
+        this.vehicle.on('response', () => {
+
+            // Whenever we get a response, reset the timer
+            if (this.switch.getValue()) {
+                this.debug('Response from Tesla API, resetting ping timer.');
+                this.timer.setTimer(this.timerInterval, this.ping.bind(this));
+            }
+            else
+                this.timer.cancel();
+
+        });
+
+    }
+
+    ping() {
+        this.debug('Ping!');
+        return this.vehicle.getVehicleData();     
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
 
 module.exports = class extends Accessory {
 
@@ -126,3 +269,4 @@ module.exports = class extends Accessory {
 
 }
 
+*/
