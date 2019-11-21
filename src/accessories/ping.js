e82cfdd2ed261ab7c182618c3fb6d2d10b279117
meg768 <meg768@users.noreply.github.com>
2019-11-21 18:34:06 +0100

    -

--- src/accessories/ping.js
+++ src/accessories/ping.js
@@ -80,7 +80,7 @@ module.exports = class extends Accessory {
         });
 
         service.getCharacteristic(Characteristic.On).on('change', (params) => {
-            this.debug(`Ping state changed to "${JSON.stringify(params.newValue}. Current ping state is ${this.pingState}."!!!!!!`);
+            this.debug(`Ping state changed to "${JSON.stringify(params)}. Current ping state is ${this.pingState}."!!!!!!`);
         });
 
     }
