bfd9dadf61006bc8f14e4c9044b8bba7c0ba0c7a
meg768 <meg768@users.noreply.github.com>
2019-11-22 18:24:35 +0100

    -

--- src/accessories/ping.js
+++ src/accessories/ping.js
@@ -20,7 +20,7 @@ module.exports = class extends Accessory {
         super({...options, config:{...defaultConfig, ...config}});
 
         this.pingState              = false;
-        this.requiredBatteryLevel   = config.requiredBatteryLevel;
+        this.requiredBatteryLevel   = 90; //config.requiredBatteryLevel;
         this.timer                  = new Timer();
         this.timerInterval          = this.config.timerInterval * 60000;
 
