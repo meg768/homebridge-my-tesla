f901c8c531bd428b31f524fccea816ed8cb5b5a8
meg768 <meg768@users.noreply.github.com>
2019-11-24 21:18:25 +0100

    -

--- src/accessories/door-lock.js
+++ src/accessories/door-lock.js
@@ -6,8 +6,7 @@ module.exports = class extends Lock {
 
     constructor(options) {
         var defaultConfig = {
-            "name": "Door",
-            "enabled": true
+            "name": "Door"
         };
 
         super({config:{...defaultConfig}, ...options});
