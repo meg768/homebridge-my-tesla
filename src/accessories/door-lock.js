c451f6a9274852a2a3945555dc7c4ddeb7495aac
meg768 <meg768@users.noreply.github.com>
2019-11-24 20:14:51 +0100

    -

--- src/accessories/door-lock.js
+++ src/accessories/door-lock.js
@@ -2,6 +2,117 @@ var {Service, Characteristic} = require('../homebridge.js');
 
 var Accessory = require('../accessory.js');
 
+class Lock extends Accessory {
+
+    static UNSECURED = Characteristic.LockCurrentState.UNSECURED;
+    static SECURED   = Characteristic.LockCurrentState.SECURED;
+    static JAMMED    = Characteristic.LockCurrentState.JAMMED;
+    static UNKNOWN   = Characteristic.LockCurrentState.UNKNOWN;
+
+    constructor(options) {
+        super(options);
+
+        this.currentLockState = Lock.UNKNOWN;
+        this.targetLockState  = Lock.UNKNOWN;
+
+        var service = new Service.LockMechanism(this.name);
+        this.addService(service);
+
+        service.getCharacteristic(Characteristic.LockCurrentState).on('get', (callback) => {
+            callback(null, this.getCurrentLockState());
+        });
+
+        service.getCharacteristic(Characteristic.LockTargetState).on('get', (callback) => {
+            callback(null, this.getTargetLockState());
+        });
+
+        service.getCharacteristic(Characteristic.LockTargetState).on('set', (value, callback) => {
+
+            this.setTargetLockState(value).then(() => {
+                callback(null, this.getTargetLockState());
+
+            })        
+            .catch((error) => {
+                this.log(error);
+                callback(null);
+            })
+        });
+    }
+
+    getLockMechanismService() {
+        return this.getService(Service.LockMechanism);
+    }
+
+    updateCurrentLockState() {
+        var service = this.getLockMechanismService();
+        service.getCharacteristic(Characteristic.LockCurrentState).updateValue(this.currentLockState);
+    }
+
+    updateTargetLockState(value) {
+        var service = this.getLockMechanismService();
+        service.getCharacteristic(Characteristic.LockTargetState).updateValue(this.targetLockState);
+    }
+
+    getCurrentLockState() {
+        return this.currentLockState;
+    }
+
+    getTargetLockState() {
+        return this.targetLockState;
+    }
+
+    getLockState(state) {
+        switch(state) {
+            case Lock.SECURED: {
+                return 'SECURED';
+            }
+            case Lock.UNSECURED: {
+                return 'UNSECURED';
+            }
+        }
+
+        return 'UNKNOWN';
+    }
+
+    setTargetLockState(value) {
+        return new Promise((resolve, reject) => {
+            if (value === this.targetLockState)
+                resolve();
+            else {
+                Promise.resolve().then(() => {
+                    this.log(`Turning lock "${this.name}" to state ${this.getLockState(value)}...`);
+                    return value ? this.lock() : this.unlock();
+    
+                })
+                .then(() => {
+                    this.currentLockState = this.targetLockState = value;
+                    this.updateCurrentLockState();
+                    return Promise.resolve();
+                })
+                .then(() => {
+                    resolve();
+                })
+                .catch((error) => {
+                    reject(error);
+                })
+            }
+        });
+
+    }
+
+    lock() {
+        return Promise.resolve();
+    }
+
+    unlock() {
+        return Promise.resolve();
+
+    }
+
+
+}
+
+
 module.exports = class extends Accessory {
 
     constructor(options) {
@@ -88,5 +199,33 @@ module.exports = class extends Accessory {
             })
         });    
     }
+
+};
+
+
+
+
+module.exports = class extends Lock {
+
+    constructor(options) {
+        var defaultConfig = {
+            "name": "Door",
+            "enabled": true
+        };
+
+        var {config, ...options} = options;
+        super({...options, config:{...defaultConfig, ...config}});
+
+        this.vehicle.on('vehicleData', (data) => {       
+            this.targetLockState = this.currentLockState = (data.vehicleState.isLocked() ? Lock.SECURED : Lock.UNSECURED);
+
+            this.debug(`Updated door lock status to ${this.getLockState(this.currentLockState)}.`);
+
+            this.updateCurrentLockState();
+            this.updateTargetLockState();
+        });
+
+    }
+
 
 };
