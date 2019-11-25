b46b7e464c9387fa2b4b1ea9a1944426d73a022d
meg768 <meg768@users.noreply.github.com>
2019-11-25 20:09:14 +0100

    -

--- src/accessories/onoff.js
+++ src/accessories/onoff.js
@@ -3,41 +3,25 @@ var Service  = require('../homebridge.js').Service;
 var Characteristic  = require('../homebridge.js').Characteristic;
 var Accessory = require('../accessory.js');
 
-class CharacteristicOn extends Characteristic.On {
-
-    constructor() {
-        super(...arguments)
-    }
-}
-
-module.exports = class Switch extends Accessory {
+module.exports = class OnOff extends Accessory {
 
     constructor(options) {
 
-        var {switchType = 'switch', ...options} = options;
+        var {serviceConstructor, ...options} = options;
 
         super(options);
  
-        this.switchState = false;
-        
-        switch (switchType) {
-            case 'fan': {
-                this.switchService = new Service.Fan(this.name);
-                break;
-            }
-            case 'switch': {
-                this.switchService = new Service.Switch(this.name);
-                break;
-            }
-            default: {
-                throw new Error(`Invalid switch type "${switchType}".`);
-            }
-        }
- 
-        this.addService(this.switchService);
+        this.onOffState = false;
+        this.service = new serviceConstructor(this.name);
 
-        this.switchService.getCharacteristic(CharacteristicOn).on('set', (value, callback) => {
-            this.setSwitchState(value).then(() => {
+        if (serviceConstructor == undefined) {
+            throw new Error(`A service constructor must be defined.`);
+        }
+
+        this.addService(this.service);
+
+        this.service.getCharacteristic(Characteristic.On).on('set', (value, callback) => {
+            this.setOnOffState(value).then(() => {
             })
             .catch((error) => {
                 this.log(error);
@@ -47,15 +31,15 @@ module.exports = class Switch extends Accessory {
             })
         });
 
-        this.switchService.getCharacteristic(CharacteristicOn).on('get', (callback) => {
+        this.service.getCharacteristic(Characteristic.On).on('get', (callback) => {
             callback(null, this.getSwitchState());
         });
     }
 
-    updateSwitchState(value) {
+    updateOnOffState(value) {
 
         var updateValue = () => {
-            this.switchService.getCharacteristic(CharacteristicOn).updateValue(this.getSwitchState());
+            this.service.getCharacteristic(Characteristic.On).updateValue(this.getSwitchState());
             return Promise.resolve();
         };
 
@@ -63,7 +47,7 @@ module.exports = class Switch extends Accessory {
             return updateValue();
         }
         return new Promise((resolve, reject) => {
-            this.setSwitchState(value).then(() => {
+            this.setOnOffState(value).then(() => {
                 return updateValue();
             })
             .then(() => {
