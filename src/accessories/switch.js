0537358283628b2aefc206ab65af95c5dc41a386
meg768 <meg768@users.noreply.github.com>
2019-11-28 21:18:07 +0100

    -

--- src/accessories/switch.js
+++ src/accessories/switch.js
@@ -31,7 +31,7 @@ module.exports = class Switch extends Accessory {
  
 
         this.addService(this.switchService = new Service.Switch(this.name));
-        this.addCharacteristic(this.switchService, Characteristic.On, this.getSwitchState, this.setSwitchState);
+        this.addCharacteristic(this.switchService, Characteristic.On, this.getSwitchState.bind(this), this.setSwitchState.bind(this));
 /*
         this.switchService.getCharacteristic(Characteristic.On).on('set', (value, callback) => {
             this.setSwitchState(value).then(() => {
