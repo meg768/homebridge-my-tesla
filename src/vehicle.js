de0fe24ff918b8b100c8496cf63c27a46479850d
meg768 <meg768@users.noreply.github.com>
2019-12-08 00:33:38 +0100

    -

--- src/vehicle.js
+++ src/vehicle.js
@@ -88,6 +88,9 @@ module.exports = class Vehicle extends TeslaAPI  {
 //        var processLoginOptions = {username:process.env.TESLA_USER, password:process.env.TESLA_PASSWORD, clientID:process.env.TESLA_CLIENT_ID, clientSecret:process.env.TESLA_CLIENT_SECRET};
         var loginOptions = {...processLoginOptions, ...configLoginOptions};
 
+        console.log('ööödfkjhsdflkjgh')
+        console.log(configLoginOptions);
+        console.log('ööödfkjhsdflkjgh')
         return super.login(configLoginOptions);
     }
 
