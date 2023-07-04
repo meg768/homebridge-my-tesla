d46b183364471e17788fd6e821bfb189f16e3738
meg768 <meg768@users.noreply.github.com>
2023-07-04 17:24:21 +0200

    -

--- src/vehicle.js
+++ src/vehicle.js
@@ -1,5 +1,4 @@
-//var TeslaAPI = require('./tesla-api-request.js');
-var TeslaAPI = require('./tesla-api-request.js');
+var TeslaAPI = require('./tesla-api.js');
 var Events = require('events');
 var {Service, Characteristic} = require('./homebridge.js');
 var Timer = require('yow/timer');
